f4ab8b053103c98578806ef5374a1d03bdbd5e90
Xmanliujun <644240319@qq.com>
2013-03-25 14:59:38 +0800

    Revert "随机数"
    This reverts commit cd2de8d14ece7f5705cb853a19f8fb8017e217d3.

--- DEMO\/AppDelegate.h
+++ DEMO\/AppDelegate.h
@@ -1,3 +1,11 @@
+//
+//  AppDelegate.h
+//  DEMO\
+//
+//  Created by kimpeng on 13-3-16.
+//  Copyright (c) 2013年 kimpeng. All rights reserved.
+//
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- DEMO\/Helper.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface Helper : NSObject
-
-+ (void)showContentViewAnimation:(UIView *)view withAlpha:(CGFloat)alpha;
-
-@end
